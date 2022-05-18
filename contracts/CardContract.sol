00c5adf5b3dd6ffc30f8979ab83236102999fea1
shohagch <shohag.mia@selise.ch>
2022-05-18 14:16:16 +0600

    gitignorNode Modules

--- contracts/CardContract.sol
+++ contracts/CardContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "./ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
--- migrations/2_ERC721_migration.js
+++ migrations/2_ERC721_migration.js
@@ -1,5 +1,5 @@
 const ERC721 = artifacts.require("ERC721");
 
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(ERC721, "SELISE xBDT", "xBDT");
+  //deployer.deploy(ERC721, "SELISE xBDT", "xBDT");
 };
